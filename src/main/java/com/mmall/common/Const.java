e79902a9833c28ce57f29fb937670d33457547e7
qlq19225 <1512830674@qq.com>
2018-10-23 23:15:59 +0800

    add backen and categoey

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -1,5 +1,9 @@
 package com.mmall.common;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
 public class Const {
     public static final String CURRENT_USER = "currentUser";
 
@@ -9,5 +13,27 @@ public class Const {
     public interface Role{
         int ROLE_CUSTOMER = 0;//nomal user
         int ROLE_ADMIN = 1;//admin
+    }
+
+    public interface ProductListOrderBy{
+        Set<String> PRICE_ASC_DESC = Sets.newHashSet("price_desc","price_asc");
+    }
+
+    public enum ProductStatusEnum{
+        ON_SALE(1,"在线");
+        private String value;
+        private int code;
+        ProductStatusEnum(int code,String value){
+            this.code = code;
+            this.value =value;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getValue() {
+            return value;
+        }
     }
 }
